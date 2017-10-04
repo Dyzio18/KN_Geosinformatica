16aa6c7c4e08cc57d571ed46c321f92be19aa42d
dyzio18 <dyzio18@gmail.com>
2017-10-05 00:33:52 +0200

    update

--- js/script.js
+++ js/script.js
@@ -11,6 +11,19 @@ $(document).ready(function() {
     // Return current year in footer
     document.getElementById('currentYear').innerHTML = (new Date()).getFullYear();
 
+    // Navigation scrolling to section
+    $("#nav__to__Contact").click(function() {
+        $('html, body').animate({ scrollTop: $("#Contact").offset().top }, 2000);
+    });
+    $("#nav__to__Project").click(function() {
+        $('html, body').animate({ scrollTop: $("#Projects").offset().top }, 2000);
+    });
+    $("#nav__to__AboutUs").click(function() {
+        $('html, body').animate({ scrollTop: $("#AboutUs").offset().top }, 2000);
+    });
+    $("#nav__to__News").click(function() {
+        $('html, body').animate({ scrollTop: $("#News").offset().top }, 2000);
+    });
 
 
 });
